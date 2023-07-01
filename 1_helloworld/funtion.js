12407110650709b134a1947a487b280ebd3828e3
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-07-01 00:53:30 -0300

    Mudando a cor do hello world

--- 1_helloworld/funtion.js
+++ 1_helloworld/funtion.js
@@ -1,4 +1,12 @@
 function Swap(){
-    document.getElementById("text").style.color = '#FF0000'
-    document.getElementById("text2").style.color = '#0000FF'
+
+    var color = document.getElementById('text').style.color;
+
+    if(color === 'red' || color === '#ff0000' || color === 'rgb(255, 0, 0)'){
+        document.getElementById("text").style.color = '#000000'
+        document.getElementById("text2").style.color = '#000000'
+    } else {
+        document.getElementById("text").style.color = '#FF0000'
+        document.getElementById("text2").style.color = '#0000FF'
+    }
 }
