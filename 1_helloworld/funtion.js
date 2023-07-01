169112763320c648a2a48bed13ed0da6b5f4da3a
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-07-01 00:03:25 -0300

    Primeiro programa

---
+++ 1_helloworld/funtion.js
@@ -0,0 +1,4 @@
+function Swap(){
+    document.getElementById("text").style.color = '#FF0000'
+    document.getElementById("text2").style.color = '#0000FF'
+}
---
+++ 1_helloworld/script.js
@@ -0,0 +1,4 @@
+let presentation = 'Hello World !'
+
+document.getElementById("text").innerHTML = presentation
+document.getElementById("text2").innerHTML = 'Welcome to the World !'
