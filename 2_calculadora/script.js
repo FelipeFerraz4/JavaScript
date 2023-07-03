2c1f4caa1e549b7fc1d04938972977ab4cc6f9a9
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-07-02 23:51:57 -0300

    estrutura básica da soma e subtração

--- 2_calculadora/script.js
+++ 2_calculadora/script.js
@@ -1,4 +1,8 @@
 let text = 0
+let camp1 = 0
+let camp2 = 0
+let operation_a_process = 0
+
 
 function number(number){
     if (document.getElementById('text').innerHTML === 'Calculadora'){
@@ -6,4 +10,24 @@ function number(number){
     }
 
     document.getElementById('text').innerHTML = document.getElementById('text').innerHTML + number
+}
+
+function sum(){
+    if (document.getElementById('text').innerHTML === 'Calculadora'){
+    }
+    else{
+        document.getElementById('text').innerHTML = document.getElementById('text').innerHTML + '+'
+    }
+}
+
+function subtraction(){
+    if (document.getElementById('text').innerHTML === 'Calculadora'){
+    }
+    else{
+        document.getElementById('text').innerHTML = document.getElementById('text').innerHTML + '-'
+    }
+}
+
+function result(){
+
 }
